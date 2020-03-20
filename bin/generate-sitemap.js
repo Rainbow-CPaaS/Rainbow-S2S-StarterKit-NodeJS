3b4a20eaeceede8ae306b013167be46a20e18cc2
Tusc Nursty <isaac@timetis.com>
2020-03-20 14:16:12 +0100

    Update : fix sitemap generation

--- bin/generate-sitemap.js
+++ bin/generate-sitemap.js
@@ -2,19 +2,23 @@
 'use strict'
 
 const yaml = require('js-yaml');
-const fs = require('fs');
 const sm = require('sitemap');
 const path = require('path');
-const hostname = 'https://@hub_fqdn@';
+const hostname = 'https://hub_fqdn';
 const pathPrefix = '/#/documentation/';
+const replaceInFile = require('replace-in-file');
+
+var fs  = require('fs');
+const { createReadStream,createWriteStream } = require('fs');
+const { SitemapStream } = require('sitemap');
 
 let docDir = `${__dirname}${path.sep}..${path.sep}doc${path.sep}sdk${path.sep}s2s-starterkit-nodejs`;
 
-let appendUrl = function (obj, stack, cb) {
+let writeUrlFromIndexYmlObjectToSitemapWriteStream = function (obj, stack, cb) {
     for (var property in obj) {
         if (obj.hasOwnProperty(property)) {
             if (typeof obj[property] === 'object') {
-                appendUrl(obj[property], stack + '.' + property, cb);
+                writeUrlFromIndexYmlObjectToSitemapWriteStream(obj[property], stack + '.' + property, cb);
             } else {
                 cb(obj[property]);
             }
@@ -23,33 +27,32 @@ let appendUrl = function (obj, stack, cb) {
 };
 
 try {
-    let args = process
-        .argv
-        .slice(2);
+   let args = process.argv.slice(2);
 
-    const index = yaml.safeLoad(fs.readFileSync(docDir+'/index.yml', 'utf8'));
+   let myhostname=(args.length > 0 && args[0])?args[0]:hostname;
+   const sitemap = new SitemapStream({ hostname: myhostname });
+   const writeStream = createWriteStream(`${docDir}/sitemap.xml`);
+   sitemap.pipe(writeStream);
+	
+   const indexYmlObject = yaml.safeLoad(fs.readFileSync(`${docDir}/index.yml`, 'utf8'));
 
-    let sitemap = sm.createSitemap({
-        hostname: (args.length && args[0])
-            ? args[0]
-            : hostname
-    });
-
-    appendUrl(index, '', (value) => {
-        sitemap.add({
-            url: value.replace(/(ref:)?(.*).md/g,pathPrefix + '$2')
-        });
-    });
-
-    sitemap.toXML(function (err, xml) {
-        if (!err) {
-            fs.writeFileSync(docDir+'/sitemap.xml', xml);
-        }
-        else
-        {
-          console.log('sitemap.toXML FAILED:' + err);
-        }
-    });
+   writeUrlFromIndexYmlObjectToSitemapWriteStream(indexYmlObject, '', (value) => {
+       sitemap.write({
+           url: value.replace(/(ref:)?(.*).md/g,pathPrefix + '$2')
+       });
+   });
+   sitemap.end();
+   (async()=>{
+   await new Promise(resolve => setTimeout(resolve, 10000));	
+   const result = replaceInFile.sync({
+	   files: `${docDir}/sitemap.xml`,
+	   encoding: 'utf8',
+	   from: /\bhub_fqdn\b/g,
+	   to: '@hub_fqdn@',
+	   countMatches: true,
+   }); 
+   console.log(result);
+   })();
 } catch (e) {
     console.log(e);
 }
